f8ded554a2f722dca672ef7f5fbe0eaa6c9aeb09
Ibuki Takazawa <2111140011u@ed.fuk.kindai.ac.jp>
2023-11-22 08:55:11 +0000

    controllerを作成した

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new
+    if @book.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 end
--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,16 +1,39 @@
 class BookshelvesController < ApplicationController
   def index
+    @bookshelves = Bookshelf.all
   end
 
   def new
+    @bookshelf = Bookshelf.new
   end
 
   def create
+    @bookshelf = Bookshelf.new(bookshelf_params)
+    if @bookshelf.save
+      redirect_to bookshelves_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @bookshelf = Bookshelf.find(params[:id])
+    if @bookshelf.update(bookshelf_params)
+      redirect_to bookshelf_path(@bookshelf)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    bookshelf = Bookshelf.find(params[:id])
+    bookshelf.destroy
+    redirect_to bookshelves_path
+  end
+  
+  private
+
+  def bookshelf_params
+    params.require(:bookshelf).permit(:shelfname, :user_id)
   end
 end
