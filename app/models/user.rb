4473258f2d66ce8bdac0ab3d013d5a80719a8bb1
Ureshino Misaki <2111140040a@ed.fuk.kindai.ac.jp>
2023-12-06 06:23:24 +0000

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :bookshelves
     has_many :histories, dependent: :destroy
     has_many :likes, dependent: :destroy
-    has_many :like_books, source: :books, through: :likes
+    has_many :like_books, source: :book, through: :likes
 end
